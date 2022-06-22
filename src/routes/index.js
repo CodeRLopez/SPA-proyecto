305d5015bf29c3603a0c58f3bb744d00d380ca0a
CodeRLopez <rlopez@nicarao.agency>
2022-06-22 15:22:14 -0600

    Agregue carpeta de getData e introducimos datos traidos de la API de cada personaje

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,15 +7,20 @@ import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
     '/': Home,
-    '/: id': Character,
+    '/:id': Character,
     '/contact': 'Contact',
 };
 
 const router = async () => {
-    const header = null || document.getElementById('header');
-    const content = null || document.getElementById('content');
+    const header = null || document.querySelector('#header');
+    const content = null || document.querySelector('#content');
 
     header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+    content.innerHTML = await render();
+
 };
 
 export default router;
